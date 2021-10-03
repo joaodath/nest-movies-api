82177deba18e136c32fefe4c45a0342a70da1c64
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-10-03 18:03:37 -0300

    feat: add initial resource files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { GenreModule } from './genre/genre.module';
+import { StaffModule } from './staff/staff.module';
 
 @Module({
-  imports: [],
+  imports: [MovieModule, PrismaModule, GenreModule, StaffModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/movie/dto/create-movie.dto.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  img: string;
+
+  @IsNotEmpty()
+  release_date: string;
+
+  @IsNotEmpty()
+  duration: number;
+
+  @IsNotEmpty()
+  genre: [number];
+
+  @IsNotEmpty()
+  staff: [number];
+}
